373a37cbf8db23bf1a30fcbe8bc62de86a9c7b2d
jobscale <jobscale@users.noreply.github.com>
2022-07-01 21:43:12 +0900

    jp bug on Debian bullseye

--- scripts/npm-update.sh
+++ scripts/npm-update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
-which jp || npm i -g @jobscale/jp
+# which jp || npm i -g @jobscale/jp
+which jq || sudo apt update && sudo apt install -y jq
 
 update() {
   for NAME in ${LIST[@]}
@@ -13,13 +14,15 @@ update() {
 {
   rm -fr node_modules package-lock.json
 
-  LIST=$(cat package.json | jp -u "keys(dependencies).join(' ', @)")
+  # LIST=$(cat package.json | jp -u "keys(dependencies).join(' ', @)")
+  LIST=$(cat package.json | jq .dependencies | jq 'keys' | jq --raw-output 'join(" ")')
   LATEST=()
   update
   time npm i ${LATEST[@]}
   echo "UPDATED '${LIST[@]}'"
 
-  LIST=$(cat package.json | jp -u "keys(devDependencies).join(' ', @)")
+  # LIST=$(cat package.json | jp -u "keys(devDependencies).join(' ', @)")
+  LIST=$(cat package.json | jq .devDependencies | jq 'keys' | jq --raw-output 'join(" ")')
   LATEST=()
   update
   time npm i -D ${LATEST[@]}
